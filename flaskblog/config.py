4c8a7d0e56a368d9e01803693342b60ae7315187
xieweijie <tianjiangjianpo@126.com>
2019-11-06 16:12:17 +0800

    Add all files in my repository

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = '72c2dfe7e0673fdb7f4aadb7fb623636'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = '345280760@qq.com'
+    MAIL_PASSWORD = 'bhsgbvenbvunbjef'
